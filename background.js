f95071ed2ded198a728a8b544b247e670fbdedf2
archit rathi <architrathi10@gmail.com>
2015-11-08 09:16:48 -0500

    added more funcs

--- background.js
+++ background.js
@@ -1,21 +1,63 @@
-"use strict";
+//"use strict";
+
+
+$.ajaxSetup({
+ async: false
+});
+
+var names = [];
+var ids = [];
+
+$.get( "http://api.reimaginebanking.com/customers?key=20496afd86b44b0ec2e8631b6947d265", function( data ) {
+  //console.log(data);
+  for(var i=0; i<data.length; i++){
+    ids[i] = data[i]._id;
+    names[i] = data[i].first_name;
+  }
+});
+
+  function findId(name){
+    for(var i=0; i<ids.length; i++){
+      if(name[i]==name){
+        return ids[i];
+      }
+    }
+  }
+
 
 if (annyang) {
   // Let's define our first command. First the text we expect, and then the function it should call
   var commands = {
-    'Google': function() {
-    chrome.tabs.create({ url: "http://google.com"});
-    console.log("hello");
-    },
 
-        'Facebook': function() {
-    chrome.tabs.create({ url: "http://facebook.com"});
-    },
-
-    'show me *term': function(term){
+    'go to *term': function(term){
           var url1="http://"+term+".com";
+          console.log(url1);
           chrome.tabs.create({ url: url1});
-    }
+    },
+
+    'deposit :money in :account': function(money, account){
+      //console.log(money+" "+account);
+      $.post( "http://api.reimaginebanking.com/accounts/"+money+"/deposits?key=20496afd86b44b0ec2e8631b6947d265", function( data ) {
+        console.log(data);
+    });
+    },
+
+    'go back': function(){
+      console.log("swag");
+
+
+      chrome.history.search({text: '', maxResults: 1}, function(data) {
+          data.forEach(function(page) {
+              chrome.runtime.sendMessage({
+                greeting: "back",
+                url: page.url
+              });
+        });
+    });
+}
+
+
+    
 
   };
 
@@ -29,4 +71,3 @@ if (annyang) {
   // Start listening. You can call this here, or attach this call to an event, button, etc.
   annyang.start();
 }
-console.log("what up");
--- popup.js
+++ popup.js
@@ -1,4 +1,10 @@
-var bg = chrome.extension.getBackgroundPage();
-
-
-
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.greeting == "back"){
+    	chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+      chrome.tabs.update(tab.id, {url: request.url});
+  })
+}
+});
+    
+  
