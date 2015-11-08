74790b0560f44c2924f7cbb193d633a94222ee34
archit rathi <architrathi10@gmail.com>
2015-11-07 20:57:16 -0500

    added show me

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+"use strict";
+
 if (annyang) {
   // Let's define our first command. First the text we expect, and then the function it should call
   var commands = {
@@ -8,9 +10,18 @@ if (annyang) {
 
         'Facebook': function() {
     chrome.tabs.create({ url: "http://facebook.com"});
+    },
+
+    'show me *term': function(term){
+          var url1="http://"+term+".com";
+          chrome.tabs.create({ url: url1});
     }
 
   };
+
+
+
+  
 
   // Add our commands to annyang
   annyang.addCommands(commands);
@@ -18,5 +29,4 @@ if (annyang) {
   // Start listening. You can call this here, or attach this call to an event, button, etc.
   annyang.start();
 }
-
 console.log("what up");
